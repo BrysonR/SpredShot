8e3031f2408ad102f961fd149327186bda0a79cd
bryson-reynolds <bryson@brysonreynolds.com>
2015-04-30 00:12:26 -0500

    getting the scraper working. fuck yes

--- scraper/ar15/ar15/settings.py
+++ scraper/ar15/ar15/settings.py
@@ -16,29 +16,29 @@ SPIDER_MODULES = ['ar15.spiders']
 NEWSPIDER_MODULE = 'ar15.spiders'
 
 ITEM_PIPELINES = [
-    'project_name.pipelines.MessageQueuePipeline',
+    'ar15.pipelines.MessageQueuePipeline',
 ]
- 
+
 try:
     BROKER_HOST = os.environ['BROKER_HOST']
 except KeyError:
     BROKER_HOST = 'rabbit'
- 
+
 try:
     BROKER_PORT = os.environ['BROKER_PORT']
 except KeyError:
     BROKER_PORT = 5672
- 
+
 try:
     BROKER_USERID = os.environ['BROKER_USERID']
 except KeyError:
     BROKER_USERID = 'guest'
- 
+
 try:
     BROKER_PASSWORD = os.environ['BROKER_PASSWORD']
 except KeyError:
     BROKER_PASSWORD = 'guest'
- 
+
 try:
     BROKER_VIRTUAL_HOST = os.environ['BROKER_VIRTUAL_HOST']
 except KeyError:
