14d3cb0f3130b1cb57d8a54446000c90015acebf
brent-mills <mills.brent@gmail.com>
2015-04-29 22:39:22 -0500

    switching to rabbit connections instead of elastic - lots of things are currently hardcoded

--- scraper/ar15/ar15/settings.py
+++ scraper/ar15/ar15/settings.py
@@ -8,20 +8,41 @@
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
 
+import os
+
 BOT_NAME = 'ar15'
 
 SPIDER_MODULES = ['ar15.spiders']
 NEWSPIDER_MODULE = 'ar15.spiders'
 
 ITEM_PIPELINES = [
-  'scrapyelasticsearch.scrapyelasticsearch.ElasticSearchPipeline',
+    'project_name.pipelines.MessageQueuePipeline',
 ]
-
-ELASTICSEARCH_SERVER = 'http://elastic/'
-ELASTICSEARCH_PORT = 9200
-ELASTICSEARCH_INDEX = 'ar15'
-ELASTICSEARCH_TYPE = 'listing'
-ELASTICSEARCH_UNIQ_KEY = 'guid'
+ 
+try:
+    BROKER_HOST = os.environ['BROKER_HOST']
+except KeyError:
+    BROKER_HOST = 'rabbit'
+ 
+try:
+    BROKER_PORT = os.environ['BROKER_PORT']
+except KeyError:
+    BROKER_PORT = 5672
+ 
+try:
+    BROKER_USERID = os.environ['BROKER_USERID']
+except KeyError:
+    BROKER_USERID = 'guest'
+ 
+try:
+    BROKER_PASSWORD = os.environ['BROKER_PASSWORD']
+except KeyError:
+    BROKER_PASSWORD = 'guest'
+ 
+try:
+    BROKER_VIRTUAL_HOST = os.environ['BROKER_VIRTUAL_HOST']
+except KeyError:
+    BROKER_VIRTUAL_HOST = '/'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'ar15 (+http://www.yourdomain.com)'
